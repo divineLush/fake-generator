222107e7c4bc335caaa2293d8e860bb032540b7b
Алексей <homeoflucifer@gmail.com>
2020-06-10 23:57:26 +0700

    little refactor

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-
-const click = async () => {
+const onClick = async () => {
     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
     const apiUrl = "https://api.namefake.com/english-united-states/female/";
     const url = proxyUrl + apiUrl;
-    const json = await fetchAsync(url);
-    console.log(json);
+    const res = await (async () => {
+        const response = await fetch(url);
+
+        return response.json();
+    })();
+    console.log(res);
 }
 
-document.querySelector(".app__btn").addEventListener("click", click);
-
-
-const fetchAsync = async (url) => {
-    let response = await fetch(url);
-    return response.json();
-}
+document.querySelector(".app__btn").addEventListener("click", onClick);
 
