2207c14b895f30e947f509e353d58733a1a3745b
VladyaRazumist <koptyackov.v@yandex.ru>
2020-06-13 18:08:22 +0700

    added more properties

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const onClick = async () => {
 
         let url = apiUrl;
         if (gender)
-            url += `?gender=${gender}`;
+            url += `?gender=${genders[gender]}`;
 
         return await callApi(url)
     }
@@ -23,31 +23,56 @@ const onClick = async () => {
     const result = response.results[0];
 
     const idendtity = {
-        email : result.email,
-        city : result.location.city,
-        country : result.location.country,
-        state : result.location.state,
-        name : {
-            first : result.name.first,
-            last : result.name.last
+        gender: result.gender,
+        email: result.email,
+        location: {
+            city: result.location.city,
+            country: result.location.country,
+            state: result.location.state,
+            postcode: result.location.postcode
         },
-        phone : result.phone
+        name: {
+            first: result.name.first,
+            last: result.name.last
+        },
+        login: {
+            username: result.login.username,
+            password: result.login.password,
+        },
+        dob: {
+            date: new Date(result.dob.date).toLocaleDateString(),
+            age: result.dob.age
+        },
+        nationality: result.nat,
+        phone: result.phone
     }
 
     document.querySelector(resultsContainerClass).innerHTML = '';
-    appendProperties(idendtity);
+    appendIdentityProperties(idendtity);
     console.log(response);
 }
 
-const appendProperties = (obj) => {
-    Object.keys(obj).forEach(key => {
-        if (typeof obj[key] === 'object')
-            appendProperties(obj[key])
-        else
-            document
-                .querySelector(resultsContainerClass)
-                .insertAdjacentHTML('beforeend', `<p><strong>${key}</strong> : ${obj[key]}</p>`);
-    })
+const appendIdentityProperties = (idendtity) => {
+    appendPropery("First Name", idendtity.name.first);
+    appendPropery("Last Name", idendtity.name.last);
+    appendPropery("Date of birth", idendtity.dob.date);
+    appendPropery("Age", idendtity.dob.age);
+    appendPropery("Nationality", idendtity.nationality);
+    appendPropery("City", idendtity.location.city);
+    appendPropery("Country", idendtity.location.country);
+    appendPropery("State", idendtity.location.state);
+    appendPropery("Postcode", idendtity.location.postcode);
+    appendPropery("E-mail", idendtity.email);
+    appendPropery("Phone", idendtity.phone);
+    appendPropery("Login", idendtity.login.username);
+    appendPropery("Password", idendtity.login.password);
+}
+
+
+const appendPropery = (key, value) => {
+    document
+        .querySelector(resultsContainerClass)
+        .insertAdjacentHTML('beforeend', `<p><strong>${key}</strong> : ${value}</p>`);
 }
 
 const genders = {
