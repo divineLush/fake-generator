813f5206806f65abdfff155cd7071a7df2f4fa12
divineLush <homeoflucifer@gmail.com>
2020-06-15 15:25:47 +0700

    resources are placed in src folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,19 @@ const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
  
 gulp.task('babel', () =>
-    gulp.src('main.js')
+    gulp.src('./src/main.js')
         .pipe(babel())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('../dist'))
 )
 
 gulp.task('copyHTML', () =>
-    gulp.src('index.html')
-        .pipe(gulp.dest('dist'))
+    gulp.src('./src/index.html')
+        .pipe(gulp.dest('../dist'))
 )
 
 gulp.task('copyCSS', () =>
-    gulp.src('main.css')
-        .pipe(gulp.dest('dist/'))
+    gulp.src('./src/main.css')
+        .pipe(gulp.dest('../dist'))
 )
 
 gulp.task('uglify', () =>
