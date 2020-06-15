db09446af2d5163917f3f2584ff68984daef27d6
divineLush <homeoflucifer@gmail.com>
2020-06-15 15:39:08 +0700

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,20 @@
 const gulp = require('gulp')
-const uglify = require('gulp-uglify')
-const babel = require('gulp-babel')
+// const uglify = require('gulp-uglify')
+// const babel = require('gulp-babel')
  
-gulp.task('babel', () =>
+gulp.task('copyJS', () =>
     gulp.src('./src/main.js')
-        .pipe(babel())
-        .pipe(gulp.dest('../dist'))
+        .pipe(gulp.dest('dist'))
 )
 
 gulp.task('copyHTML', () =>
     gulp.src('./src/index.html')
-        .pipe(gulp.dest('../dist'))
+        .pipe(gulp.dest('dist'))
 )
 
 gulp.task('copyCSS', () =>
     gulp.src('./src/main.css')
-        .pipe(gulp.dest('../dist'))
+        .pipe(gulp.dest('dist'))
 )
 
-gulp.task('uglify', () =>
-    gulp.src('dist/main.js')
-        .pipe(uglify())
-)
-
-gulp.task('default', gulp.series('copyHTML', 'copyCSS', 'babel'))
+gulp.task('default', gulp.series('copyHTML', 'copyCSS', 'copyJS'))
